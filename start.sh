c449ca90b96ca4f4c61272bfe17e8c74b9294d56
Trottero <wrlxs@hotmail.com>
2020-12-17 16:27:22 +0100

    Update start iwth actual command

--- start.sh
+++ start.sh
@@ -5,10 +5,8 @@ cat ${SRC_PATH}hosts | while read worker;
 do
   echo "Starting workers: ${worker}"
   wrk=($(echo $worker | tr ":" " "))
-  echo ${wrk[@]}
-  echo ${wrk[0]}
-  (echo "" | ssh ${wrk[0]} dir) &
-  # (echo "" | ssh ${wrk[0]} 'python ${SRC_PATH}server.py ${wrk[@]}') &
+
+  (echo "" | ssh ${wrk[0]} python ${SRC_PATH}server.py ${wrk[@]}) &
   # (python ${SRC_PATH}server.py ${wrk[@]} ss) &
 done
 
