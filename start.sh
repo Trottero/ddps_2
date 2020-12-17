17ee09ad19ff54efdf55efc0cbb1f79ade6bd981
Trottero <wrlxs@hotmail.com>
2020-12-17 16:15:09 +0100

    change start script

--- start.sh
+++ start.sh
@@ -4,8 +4,9 @@ SRC_PATH = ~/source/repos/ddps_2/
 cat ${SRC_PATH}hosts | while read worker;
 do
   echo "Starting workers: ${worker}"
-  wrk=$(echo $worker | tr ":" "\n")
-  (echo "" | ssh ${wrk[0]} "python ${SRC_PATH}server.py ${wrk[@]}") &
+  wrk=$(echo $worker | tr ":" " ")
+  echo ${wrk[@]}
+  (echo "" | ssh ${wrk[0]} 'python ${SRC_PATH}server.py ${wrk[@]}') &
   # (python ${SRC_PATH}server.py ${wrk[@]} ss) &
 done
 
