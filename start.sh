a2e91c3f045bbe1262b117354147a8cf62a2007e
Trottero <wrlxs@hotmail.com>
2020-12-17 16:07:31 +0100

    Update start script

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ cat ${SRC_PATH}hosts | while read worker;
 do
   echo "Starting workers: ${worker}"
   wrk=$(echo $worker | tr ":" "\n")
-  (echo "" | ssh ${wrk[0]} python ${SRC_PATH}server.py ${wrk[@]}) &
+  (echo "" | ssh ${wrk[0]} "python ${SRC_PATH}server.py ${wrk[@]}") &
   # (python ${SRC_PATH}server.py ${wrk[@]} ss) &
 done
 
