b386964304f815867e4b651014414ac3ab6145d1
Trottero <wrlxs@hotmail.com>
2020-12-17 16:32:16 +0100

    Update start script by specificying python version

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ do
   echo "Starting workers: ${worker}"
   wrk=($(echo $worker | tr ":" " "))
 
-  (echo "" | ssh ${wrk[0]} python ${SRC_PATH}server.py ${wrk[@]}) &
+  (echo "" | ssh ${wrk[0]} python3 ${SRC_PATH}server.py ${wrk[@]}) &
   # (python ${SRC_PATH}server.py ${wrk[@]} ss) &
 done
 
