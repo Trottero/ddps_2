1f8ba9756d64f857938fc0bfd06371477474c8ff
Trottero <wrlxs@hotmail.com>
2020-12-18 23:36:14 +0100

    Up threads and update hosts

--- start.sh
+++ start.sh
@@ -6,8 +6,8 @@ do
   echo "Starting workers: ${worker}"
   wrk=($(echo $worker | tr ":" " "))
 
-  # (echo "" | ssh ${wrk[0]} "cd ${SRC_PATH} && module load python/3.6.0 && python3 -m pip install -r ${SRC_PATH}requirements.txt --user && python3 server.py ${wrk[@]} log &") &
-  (python server.py ${wrk[@]} ss) &
+  (echo "" | ssh ${wrk[0]} "cd ${SRC_PATH} && module load python/3.6.0 && python3 -m pip install -r ${SRC_PATH}requirements.txt --user && python3 server.py ${wrk[@]} log &") &
+  # (python server.py ${wrk[@]} ss) &
 done
 
 echo "Done!"
