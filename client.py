ce4f5a03d11a7710f34563e4c4db5cd6e1d942a0
Trottero <wrlxs@hotmail.com>
2020-12-23 21:54:09 +0100

    Lock the seed for the client

--- client.py
+++ client.py
@@ -6,7 +6,9 @@ of the chord protocol.
 from __future__ import print_function
 import logging
 import benchmarks
+import numpy as np
 
 if __name__ == '__main__':
+    np.random.seed(42)
     logging.basicConfig()
     benchmarks.benchmark_performance_writes()
